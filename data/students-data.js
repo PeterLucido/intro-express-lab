559368841703b8cc55aa647399e3aecab8f72809
Peter Lucido <petermlucido@gmail.com>
2023-04-11 18:26:05 -0500

    Finish putting in students

---
+++ data/students-data.js
@@ -0,0 +1,16 @@
+const students = [
+  {text: 'Peter', done: true, _id: 125223},
+  {text: 'Ryan', done: false, _id: 127904},
+  {text: 'Sean', done: false, _id: 139608},
+  {text: 'Tom', done: false, _id: 139609},
+  {text: 'Joe', done: false, _id: 139610},
+  {text: 'Patty', done: false, _id: 139611},
+  {text: 'Steve', done: false, _id: 139612},
+  {text: 'Conall', done: false, _id: 139613},
+  {text: 'Patrick', done: false, _id: 139614},
+  {text: 'Danny', done: false, _id: 139615},
+]
+
+export { 
+	students
+}
